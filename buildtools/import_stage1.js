a4bac00298003a56dc780766364b7bfe1234b3d5
ChooChooShoe <timothy.durbano@gmail.com>
2019-10-25 17:33:00 -0400

    Updated to patch 9.21.1

--- buildtools/import_stage1.js
+++ buildtools/import_stage1.js
@@ -16,18 +16,22 @@ const request = require('request');
 const he = require('he');
 
 const noReedoHrefs = new Set();
-function fetch_wikia(url, callback) {
-  console.log("Fetching (chomp): %s", url);
-  request(url, (err, res, body) => {
-    if (err) return console.warn('Bad request: ' + err.toString());
-    // if (noReedoHrefs.has(res.href)) return console.warn('Url is the same as one before. skipping ' + res.href)
-    // noReedoHrefs.add(res.href);
-    const content = body.match(/<textarea[^>]*>([\s\S]*)<\/textarea>/m);
-    if (!content)
-      return console.warn('Unknown page found for ' + url);
-    callback(he.decode(content[1]));
+function fetch_wikia(url) {
+  return new Promise((resolve, reject) => {
+    console.log("Fetching (chomp): %s", url);
+    request(url, (error, response, body) => {
+      if (error) reject(error);
+      if (response.statusCode != 200) {
+        reject('Invalid status code <' + response.statusCode + '>');
+      }
+      const content = body.match(/<textarea[^>]*>([\s\S]*)<\/textarea>/m);
+      if (!content)
+        reject('Page does not have the corrent content. (missing textarea)' + url);
+      resolve(he.decode(content[1]));
+    });
   });
 }
+
 function make_changes(changes) {
   let nums = changes.replace('V', '').replace('b', '.2').split('.')
   if (nums.length == 2)
@@ -79,49 +83,8 @@ function autoCast(s) {
     return false;
   return s;
 }
-// might call callback on r2 before r1 is saved. 
-function chain_skills(cexport, champ_name, calback) {
-  champ_name = champ_name.replace(/ /g, '_');
-  try_take_skill('I', cexport.wikia_champ.skill_i, cexport, champ_name, () => {
-    try_take_skill('Q', cexport.wikia_champ.skill_q, cexport, champ_name, () =>
-      try_take_skill('W', cexport.wikia_champ.skill_w, cexport, champ_name, () => {
-        try_take_skill('E', cexport.wikia_champ.skill_e, cexport, champ_name, () => {
-          try_take_skill('R', cexport.wikia_champ.skill_r, cexport, champ_name, calback);
-        })
-      })
-    )
-  })
-}
 
 const noRepeatDesc = new Set();
-function try_take_skill(letter, value, cexport, champ_name, callback) {
-  if (!value) return;
-  let skillsplit = value.toString().split(";")
-  for (let skillnum = 0; skillnum < skillsplit.length; skillnum++) {
-    const isFinalCall = skillnum === skillsplit.length - 1;
-    const element = skillsplit[skillnum];
-    const skill_name = element.trim().replace(/ /g, "_")
-    const url = `https://leagueoflegends.fandom.com/wiki/Template:Data_${champ_name}/${skill_name}?action=edit`
-    fetch_wikia(url, (data) => {
-      let skillObj = wikiaSourceToObj(data);
-      let valid = true;
-      if (skillObj.description) {
-        if (noRepeatDesc.has(skillObj.description)) {
-          console.warn('Description is the same as one before. Skipping', skill_name, skillObj.description)
-          valid = false;
-        } else
-          noRepeatDesc.add(skillObj.description);
-      }
-      if (valid) {
-        skillObj.skillkey = letter.toUpperCase();
-        skillObj.skillid = letter.toLowerCase() + (skillnum + 1).toString();
-        cexport.skills[skillObj.skillid] = skillObj;
-      }
-      if (callback && isFinalCall)
-        callback();
-    });
-  }
-}
 
 if (process.argv.length <= 2) {
   console.log("WARNING champion id to download is required but not supplied");
@@ -178,7 +141,7 @@ request(realmsUrl, { json: true }, (err, res, body) => {
       const champ = body.data[keyid];
       console.log('Taking: ', keyid);
 
-      fetch_wikia(`https://leagueoflegends.fandom.com/wiki/Template:Data_${champ.name}?action=edit`, (data) => {
+      fetch_wikia(`https://leagueoflegends.fandom.com/wiki/Template:Data_${champ.name}?action=edit`).then(async (data) => {
         console.log("File got (wikia) ", champ.name);
         let rootObj = default_export(champ);
 
@@ -187,22 +150,53 @@ request(realmsUrl, { json: true }, (err, res, body) => {
         rootObj.wikia_champ = champObj
 
         rootObj.skills = {};
-        chain_skills(rootObj, champ.name, () => {
-          // champtions[keyid] = cexport
-          fs.writeFile(`./export/${keyid}.pass1.json`, JSON.stringify(rootObj, null, 2), function (err) {
-            if (err) {
-              return console.log(err);
-            }
-            console.log(`The file './export/${keyid}.pass1.json' was saved!`);
-          });
+        const champ_name = champ.name.replace(/ /g, '_');
+        await createSkill('I', rootObj.wikia_champ.skill_i, rootObj, champ_name);
+        await createSkill('Q', rootObj.wikia_champ.skill_q, rootObj, champ_name);
+        await createSkill('W', rootObj.wikia_champ.skill_w, rootObj, champ_name);
+        await createSkill('E', rootObj.wikia_champ.skill_e, rootObj, champ_name);
+        await createSkill('R', rootObj.wikia_champ.skill_r, rootObj, champ_name);
+
+        fs.writeFile(`./export/${keyid}.pass1.json`, JSON.stringify(rootObj, null, 2), function (err) {
+          if (err) {
+            return console.log(err);
+          }
+          console.log(`The file './export/${keyid}.pass1.json' was saved!`);
         });
       });
     }
   });
 })
 
+async function createSkill(letter, skillNames, rootObj, champ_name) {
+  if (!skillNames) return;
+  const skillsplit = skillNames.toString().split(";")
+  for (let skillnum = 0; skillnum < skillsplit.length; skillnum++) {
+    const element = skillsplit[skillnum];
+    const skill_name = element.trim().replace(/ /g, "_")
+    const url = `https://leagueoflegends.fandom.com/wiki/Template:Data_${champ_name}/${skill_name}?action=edit`
+    const data = await fetch_wikia(url);
+
+    let skillObj = wikiaSourceToObj(data);
+    let valid = false;
+    if (skillObj.description) {
+      if (noRepeatDesc.has(skillObj.description)) {
+        console.warn('Description is the same as one before. Skipping', skill_name, skillObj.description)
+      } else {
+        noRepeatDesc.add(skillObj.description);
+        valid = true;
+      }
+    }
+    if (valid) {
+      skillObj.skillkey = letter.toUpperCase();
+      skillObj.skillid = letter.toLowerCase() + (skillnum + 1).toString();
+      rootObj.skills[skillObj.skillid] = skillObj;
+    }
+  }
+}
+
 function wikiaSourceToObj(data) {
-  if(data.length < 3){
+  if (data.length < 3) {
     console.warn("Missing data. No data found");
     return {};
   }
@@ -213,8 +207,8 @@ function wikiaSourceToObj(data) {
     if (isFirstLine) {
       // line === "{{{{{1<noinclude>|Ability data</noinclude>}}}|Terrashape|{{{2|}}}|{{{3|}}}|{{{4|}}}|{{{5|}}}"
       const match = line.match(/noinclude>}}}\|([^|]*)\|/);
-      obj["name"] =  match[1];
-      console.log("Setting name from to",  match[1]);
+      obj["name"] = match[1];
+      console.log("Setting name to", match[1]);
       isFirstLine = false;
     } else {
       const parts = line.split('=');
